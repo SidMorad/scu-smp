973990e46738e5ef48aede044807208ff94caf1f
sid.moradi <sid.moradi@d4836a7a-25a3-11df-a76c-adfa62dee608>
2010-06-09 07:30:15 +0000

    Incomplete - task 2: Initialize Project (Python programming language) http://code.google.com/p/scu-smp/issues/detail?id=2
    * Modify database settings (settings.py)
    * Django command 'python manage.py syncdb'
    * Note Super-user username/password is sid/sid  (I picked these username and password because it is easy to enter and because it's my name too ;-) )

--- scusmp/settings.py
+++ scusmp/settings.py
@@ -1,4 +1,5 @@
 # Django settings for scusmp project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -9,10 +10,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(SITE_ROOT, 'database') + '/development_scusmp.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
