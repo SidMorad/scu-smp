0c79035dadfba726552285b04c7477f8fe3bc5c8
sid.moradi <sid.moradi@d4836a7a-25a3-11df-a76c-adfa62dee608>
2010-06-16 12:08:22 +0000

    Incomplete - task 4: Initialize Project (PHP programming language) http://code.google.com/p/scu-smp/issues/detail?id=4
    * Add ADOdb library
    * Add g960 CSS Framework 
    * Add some empty folders for future project sturcture

---
+++ trunk/smp-php/application/common/footer.php
@@ -0,0 +1,8 @@
+			
+			</div><!-- end content -->
+			<div class="grid_12" id="footer">
+				FOOTER OF PAGE
+			</div>
+		</div><!-- end container -->	
+	</body>	
+</html>
---
+++ trunk/smp-php/application/common/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<title>SMP | <?php print $pageTitle?></title>
+		<link type="text/css" rel="Stylesheet" media="all" href="static/css/reset.css" />
+		<link type="text/css" rel="Stylesheet" media="all" href="static/css/text.css" />
+		<link type="text/css" rel="Stylesheet" media="all" href="static/css/960.css" />
+	</head>	
+	<body>
+		<div class="container_12" id="container">
+			<div class="grid_12" id="header">
+				HEADER OF PAGE
+			</div>
+			<div class="grid_3" id="menu" >
+				MENU OF PAGE
+			</div>
+			<div class="grid_9" id="content" >
+			
