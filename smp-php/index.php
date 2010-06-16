897e8fef29eb576ef7cdeaae27f25e8a977a91dc
sid.moradi <sid.moradi@d4836a7a-25a3-11df-a76c-adfa62dee608>
2010-06-16 12:08:22 +0000

    Incomplete - task 4: Initialize Project (PHP programming language) http://code.google.com/p/scu-smp/issues/detail?id=4
    * Add ADOdb library
    * Add g960 CSS Framework 
    * Add some empty folders for future project sturcture

---
+++ smp-php/index.php
@@ -0,0 +1,7 @@
+<?php
+$pageTitle= "Home Page";
+include dirname(__FILE__).'/application/common/header.php';
+
+print "CONTENT OF PAGE HERE!!! + HELLO WORLD :D";
+
+include dirname(__FILE__).'/application/common/footer.php';
