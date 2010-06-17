3b896abbb0a0002019a2ae988b934f39f124a8e9
sid.moradi <sid.moradi@d4836a7a-25a3-11df-a76c-adfa62dee608>
2010-06-17 08:16:53 +0000

    Incomplete - task 3: xHtml http://code.google.com/p/scu-smp/issues/detail?id=3
    * Add menu by using The PHP Layers Menu System version 3.2.0 (http://phplayersmenu.sourceforge.net/)
    * I've configure 'only' Tree Menu for our application , we can consider other avaivble type of menus too, see the demo at http://phplayersmenu.sourceforge.net/demo.php

--- smp-php/application/common/header.php
+++ smp-php/application/common/header.php
@@ -3,9 +3,24 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
 		<title>SMP | <?php print $pageTitle?></title>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 		<link type="text/css" rel="Stylesheet" media="all" href="static/css/reset.css" />
 		<link type="text/css" rel="Stylesheet" media="all" href="static/css/text.css" />
 		<link type="text/css" rel="Stylesheet" media="all" href="static/css/960.css" />
+		<link type="text/css" rel="Stylesheet" media="all" href="static/css/menu/layersmenu-demo.css" />
+		<link type="text/css" rel="Stylesheet" media="all" href="static/css/menu/layerstreemenu.css" />
+		<style type="text/css">
+		<!--
+		@import url("static/css/layerstreemenu-hidden.css");
+		//-->
+		</style>
+		
+		<script language="JavaScript" type="text/javascript">
+		<!--
+			<?php require_once 'static/js/menu/layersmenu-browser_detection.js'; ?>
+		// -->
+		</script>
+		<script language="JavaScript" type="text/javascript" src="static/js/menu/layerstreemenu-cookies.js"></script>
 	</head>	
 	<body>
 		<div class="container_12" id="container">
@@ -13,7 +28,7 @@
 				HEADER OF PAGE
 			</div>
 			<div class="grid_3" id="menu" >
-				MENU OF PAGE
+				<?php include 'menu.php'?>
 			</div>
 			<div class="grid_9" id="content" >
 			
