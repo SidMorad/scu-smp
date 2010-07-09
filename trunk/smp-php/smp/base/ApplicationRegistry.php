dc4168794e7465efca4ea3403dbc65beed157c54
sid.moradi <sid.moradi@d4836a7a-25a3-11df-a76c-adfa62dee608>
2010-07-09 03:34:17 +0000

    Incomplete - task 4: Initialize Project (PHP programming language) http://code.google.com/p/scu-smp/issues/detail?id=4
    * Change ApplicationRepository to work with file instead of Memeory . The resoan was becuase of memeroy access issue at Windows OS.
    * Add references.txt for showing used resources at code.

--- trunk/smp-php/smp/base/ApplicationRegistry.php
+++ trunk/smp-php/smp/base/ApplicationRegistry.php
@@ -1,8 +1,9 @@
 <?php
 /**
  * Created at 06/07/2010 1:59:27 PM
- * filename
- *
+ * smp_base_ApplicationRegistry
+ * 
+ * This class is a part of Registry patterns. see Reference#1
  * @author <a href="mailto:smorad12@scu.edu.au">Sid</a>
  * @version 1.0
  */
@@ -11,16 +12,11 @@ require_once('smp/base/AppException.php');
 class smp_base_ApplicationRegistry extends smp_base_Registry {
 	private static $instance;
 	private $values = array();
-	private $id;
-	const DSN = 1;
-	const ADODB = 2;
+	private $mtimes = array();
+	//TODO $freezdir should be SECURE directory! and currently is not. 
+	private $freezdir = "smp/application_registry_data";
 	
-	private function __construct() {
-		$this->id = @shm_attach(55,10000,0600);
-		if (! $this->id) {
-			throw new smp_base_AppException("Could not access shared memeory");
-		}
-	}
+	private function __construct() {}
 	
 	static function instance() {
 		if (! isset(self::$instance)) {
@@ -30,27 +26,39 @@ class smp_base_ApplicationRegistry extends smp_base_Registry {
 	}
 		
 	protected function get($key) {
-		return shm_get_var($this->id, $key);
+		$path = $this->freezdir . DIRECTORY_SEPARATOR . $key;
+		if (file_exists($path)) {
+			clearstatcache();
+			$mtime = filemtime($path);
+			
+			if (!isset($this->mtimes[$key])) {
+				$this->mtimes[$key] = 0;
+			}
+			if ($mtime > $this->mtimes[$key]) {
+				$data = file_get_contents($path);
+				$this->mtimes[$key] = $mtime;
+				return ($this->values[$key] = unserialize($data));
+			}
+			if (isset($this->values[$key])) {
+				return $this->values[$key];
+			}
+			return null;
+		} 
 	}
 	
 	protected function set($key, $val) {
-		shm_put_var($this->id, $key, $val);
+		$this->values[$key] = $val;
+		$path = $this->freezdir .DIRECTORY_SEPARATOR . $key;
+		file_put_contents($path, serialize($val));
+		$this->mtimes[$key]=time();
 	}	
 	
 	static function getDSN() {
-		return self::instance()->get(self::DSN);
+		return self::instance()->get('dsn');
 	}
 	
 	static function setDSN($dsn) {
-		self::instance()->set(self::DSN, $dsn);
+		self::instance()->set('dsn', $dsn);
 	}
 
-	static function getADODB() {
-		return self::instance()->get(self::ADODB);
-	}
-	
-	static function setADODB($adodb) {
-		self::instance()->set(self::ADODB, $adodb);
-	}
-	
 }
---
+++ trunk/smp-php/smp/base/MemApplicationRegistry.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created at 09/07/2010 12:19:19 PM
+ * smp_base_MemApplicationRegistry
+ *
+ * This class is a part of Registry patterns. see Reference#1
+ * @author <a href="mailto:smorad12@scu.edu.au">Sid</a>
+ * @version 1.0
+ */
+require_once('smp/base/Registry.php');
+class smp_base_MemApplicationRegistry extends smp_base_Registry {
+	private static $instance;
+	private $values = array();
+	private $id;
+	const DSN = 1;
+	const ADODB = 2;
+	
+	private function __construct() {
+		$this->id = @shm_attach(55,10000,0600);
+		if (! $this->id) {
+			throw new smp_base_AppException("Could not access shared memeory");
+		}
+	}
+	
+	static function instance() {
+		if (! isset(self::$instance)) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+		
+	protected function get($key) {
+		return shm_get_var($this->id, $key);
+	}
+	
+	protected function set($key, $val) {
+		shm_put_var($this->id, $key, $val);
+	}	
+	
+	static function getDSN() {
+		return self::instance()->get(self::DSN);
+	}
+	
+	static function setDSN($dsn) {
+		self::instance()->set(self::DSN, $dsn);
+	}
+
+	static function getADODB() {
+		return self::instance()->get(self::ADODB);
+	}
+	
+	static function setADODB($adodb) {
+		self::instance()->set(self::ADODB, $adodb);
+	}
+}
