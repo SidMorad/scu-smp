4d8fb022bed7756628dafd0efe6ad5750a58d460
lsq991 <lsq991@d4836a7a-25a3-11df-a76c-adfa62dee608>
2010-08-02 05:56:47 +0000

    click mentee's mentor name to see some details

--- trunk/smp-php/smp/view/student/listMatchedMentee.php
+++ trunk/smp-php/smp/view/student/listMatchedMentee.php
@@ -20,7 +20,8 @@ if (! empty($list)) {
 	print $indent."	<th>Student Number</th>\r\n";	
 	print $indent."	<th>Course</th>\r\n";	
 	print $indent."	<th>Study Mode</th>\r\n";	
-	print $indent."	<th>Age Range</th>\r\n";	
+	print $indent."	<th>Age Range</th>\r\n";
+	print $indent."	<th>Mentor</th>\r\n";		
 	foreach ($list as $student) {
 	print $indent."	<tr>\r\n";
 		print $indent."		<td>".$student->getFirstname()."</td>\r\n";
@@ -30,7 +31,8 @@ if (! empty($list)) {
 		print $indent."		<td>".$student->getCourse()."</td>\r\n";
 		print $indent."		<td>".VH::getValueFromFixArray('study_mode', $student->getStudyMode())."</td>\r\n";
 		print $indent."		<td>".VH::getValueFromFixArray('age_range', $student->getAgeRange())."</td>\r\n";
-	print $indent."	</tr>\r\n";
+		print $indent."		<td><a href=\"index.php?cmd=student/showStudentMenteeMentor&menteeId=".$student->getId()."\">".$student->getMentor()->getFirstname()." ".$student->getMentor()->getLastname()."</a></td>\r\n";
+		print $indent."	</tr>\r\n";
 	}
 	print $indent."</table>\r\n";
 } else {
---
+++ trunk/smp-php/smp/view/student/showStudentMenteeMentor.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created at 02/08/2010 3:39:09 PM
+ * smp/view/student/showStudentMenteeMentor.php
+ *
+ * @author <a href="mailto:sli24@scu.edu.au">Bruce</a>
+ * @version 1.0
+ */
+include("smp/view/common/header.php");
+
+$student = $request->getEntity();
+
+$indent = "				";
+print $indent."<div class=\"form_container\">\r\n";
+	print $indent."	<div class=\"grid_12\">\r\n";
+	print $indent."		<label>&nbsp;</label>\r\n";
+	print $indent."	</div>\r\n";
+	print $indent."	<div class=\"grid_12\">\r\n";
+	print $indent."		<h2>Mentor's Information for '".$student->getFirstname()."	".$student->getLastname() ."' &nbsp;SN:[".$student->getStudentNumber()."]</h2>\r\n";
+	print $indent."		<hr/>\r\n";
+	print $indent."	</div>\r\n";
+	
+	$mentor=$student->getMentor();
+	print $indent."	<div class=\"grid_2\">\r\n";
+	print $indent."		<label class=\"label\">Name :</label>\r\n";
+	print $indent."	</div>\r\n";
+	print $indent."	<div class=\"grid_10\">\r\n";
+	print $indent."		<label class=\"labelValue\">".$mentor->getFirstname()."	".$mentor->getLastname()."</label>\r\n";
+	print $indent."	</div>\r\n";
+	print $indent."	<div class=\"grid_2\">\r\n";
+	print $indent."		<label class=\"label\">Student number :</label>\r\n";
+	print $indent."	</div>\r\n";
+	print $indent."	<div class=\"grid_10\">\r\n";
+	print $indent."		<label class=\"labelValue\">".VH::chN($mentor->getStudentNumber())."</label>\r\n";
+	print $indent."	</div>\r\n";
+	print $indent."	<div class=\"grid_2\">\r\n";
+	print $indent."		<label class=\"label\">Mobile :</label>\r\n";
+	print $indent."	</div>\r\n";
+	print $indent."	<div class=\"grid_10\">\r\n";
+	print $indent."		<label class=\"labelValue\">".VH::chN($mentor->getContact()->getMobile())."</label>\r\n";
+	print $indent."	</div>\r\n";
+	print $indent."	<div class=\"grid_2\">\r\n";
+	print $indent."		<label class=\"label\">Email :</label>\r\n";
+	print $indent."	</div>\r\n";
+	print $indent."	<div class=\"grid_10\">\r\n";
+	print $indent."		<label class=\"labelValue\">".$mentor->getUser()->getScuEmail()."</label>\r\n";
+	print $indent."	</div>\r\n";
+	print $indent."	<div class=\"grid_2\">\r\n";
+	print $indent."		<label class=\"label\">Course :</label>\r\n";
+	print $indent."	</div>\r\n";
+	print $indent."	<div class=\"grid_10\">\r\n";
+	print $indent."		<label class=\"labelValue\">".VH::chN($mentor->getCourse())."</label>\r\n";
+	print $indent."	</div>\r\n";
+	print $indent."	<div class=\"grid_12\">\r\n";
+	print $indent."		&nbsp;\r\n";
+	print $indent."	</div>\r\n";
+
+print $indent."</div>\r\n";
+
+include("smp/view/common/footer.php");
