48b677cc7453ebec7b01563abe0e503b7600e03c
lsq991 <lsq991@d4836a7a-25a3-11df-a76c-adfa62dee608>
2010-08-27 03:51:40 +0000

    Incomplete - Issue 20: Make all list Sortable and Searchable http://code.google.com/p/scu-smp/issues/detail?id=20
    * list of Matched Mentors are sortable and searchable now (cmd=student/listMatchedMentor)
    * Update database to use 'f' and 'm' instead of 'female' and 'male'.

--- smp-php/smp/view/student/listMatchedMentor.php
+++ smp-php/smp/view/student/listMatchedMentor.php
@@ -4,39 +4,40 @@
  * smp/view/student/listMatchedMentor.php
  *
  * @author <a href="mailto:smorad12@scu.edu.au">Sid</a>
+ * @author <a href="mailto:sli24@scu.edu.au">Bruce</a>
  * @version 1.0
  */
 include('smp/view/common/header.php');
+require_once('smp/util/OptionProvider.php');
+require_once('smp/util/DatagridUtil.php');
 
 $indent = "				";
 print $indent."<br><h1>List of Matched Mentors</h1><br>\r\n";
 
-$list = $request->getList();
-if (! empty($list)) {
-	print $indent."<table class=\"table\">\r\n";
-	print $indent."	<th>Firstname</th>\r\n";	
-	print $indent."	<th>Lastname</th>\r\n";	
-	print $indent."	<th>Gender</th>\r\n";	
-	print $indent."	<th>Student Number</th>\r\n";	
-	print $indent."	<th>Course</th>\r\n";	
-	print $indent."	<th>Study Mode</th>\r\n";	
-	print $indent."	<th>Age Range</th>\r\n";	
-	print $indent."	<th>Mentees</th>\r\n";	
-	foreach ($list as $mentor) {
-	print $indent."	<tr>\r\n";
-		print $indent."		<td>".$mentor->getStudent()->getFirstname()."</td>\r\n";
-		print $indent."		<td>".$mentor->getStudent()->getLastname()."</td>\r\n";
-		print $indent."		<td>".VH::getValueFromFixArray('gender', $mentor->getStudent()->getGender())."</td>\r\n";
-		print $indent."		<td>".$mentor->getStudent()->getStudentNumber()."</td>\r\n";
-		print $indent."		<td>".$mentor->getStudent()->getCourse()."</td>\r\n";
-		print $indent."		<td>".VH::getValueFromFixArray('study_mode', $mentor->getStudent()->getStudyMode())."</td>\r\n";
-		print $indent."		<td>".VH::getValueFromFixArray('age_range', $mentor->getStudent()->getAgeRange())."</td>\r\n";
-		print $indent."		<td><a href=\"index.php?cmd=student/showStudentMentorMentees&mentorId=".$mentor->getId()."\">".count($mentor->getMentees())."</a></td>\r\n";
-	print $indent."	</tr>\r\n";
-	}
-	print $indent."</table>\r\n";
-} else {
-	print $indent. "<p>No matched Mentor found.</p>";
-}
+include("smp/view/search/mentorSearchPanel.php");
+$datagrid=&$request->getDatagrid();
+
+//use Formatter to edit generated data
+$studyModeColumn=$datagrid->getColumnByField('study_mode');
+$studyModeColumn->setFormatter('formatStudyMode');
+$genderColumn=$datagrid->getColumnByField('gender');
+$genderColumn->setFormatter('formatGender');
+
+
+$table = smp_util_DatagridUtil::getCustomHtmlTable();
+
+$datagrid->fill($table,smp_util_DatagridUtil::getRenderOptions());
+
+print $table->toHtml();
+$datagrid->render(DATAGRID_RENDER_PAGER);
 
 include('smp/view/common/footer.php');
+
+function formatStudyMode($params){
+	$key=$params['record']['study_mode'];
+	return VH::getValueFromFixArray('study_mode', $key);
+}
+function formatGender($params){
+	$key=$params['record']['gender'];
+	return VH::getValueFromFixArray('gender', $key);
+}
