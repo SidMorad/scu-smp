82b5ab03061b3fd22f0d12dba2cb1ec2b697e1c3
sid.moradi <sid.moradi@d4836a7a-25a3-11df-a76c-adfa62dee608>
2010-09-27 06:28:14 +0000

    Incomplete - Issue 29: Automated email to mentor and mentee http://code.google.com/p/scu-smp/issues/detail?id=29
    * Add Mentor/Mentee contact information to email templates
    * Send email to Mentor after registration.

--- smp-php/smp/util/EmailTemplate.php
+++ smp-php/smp/util/EmailTemplate.php
@@ -16,7 +16,10 @@ class smp_util_EmailTemplate {
 	static function bodyForMentorAfterMatching($mentor, $mentee) {
 		$body = "Dear " . $mentor->getStudent()->getFirstname() . "\n\n";
 		$body .= "You are matched with new Mentee :". $mentee->getStudent()->getFirstname() ." " . $mentee->getStudent()->getLastname(). "\n";
-		$body .= "Please login to our website and see her/his information. \n";
+		$body .= "SCU-Email: " . $mentee->getUser()->getScuEmail() . "\n";
+		$body .= "Personal Email: " . $mentee->getContact()->getEmail() . "\n";
+		$body .= "Mobile: " . $mentee->getContact()->getMobile() . "\n";
+		$body .= "You can also login to our website and see her/his information. \n";
 		$body .= Constants::APPLICATION_DOMAIN."index.php?cmd=public/login";
 		return $body;
 	}
@@ -28,8 +31,24 @@ class smp_util_EmailTemplate {
 	static function bodyForMenteeAfterMatching($mentor, $mentee) {
 		$body = "Dear " . $mentee->getStudent()->getFirstname() . "\n\n";
 		$body .= "You are matched with your Mentor: ". $mentor->getStudent()->getFirstname() ." " . $mentor->getStudent()->getLastname(). "\n";
-		$body .= "Please login to our website and see her/his information. \n";
+		$body .= "SCU-Email: " . $mentor->getUser()->getScuEmail() . "\n";
+		$body .= "Personal Email: " . $mentor->getContact()->getEmail() . "\n";
+		$body .= "Mobile: " . $mentor->getContact()->getMobile() . "\n";
+		$body .= "You can also login to our website and see her/his information. \n";
 		$body .= Constants::APPLICATION_DOMAIN."index.php?cmd=public/login";		
+		return $body;
+	}
+	
+	static function subjectForMentorAfterRegistration() {
+		return "Registration was successfull";
+	}
+
+	static function bodyForMentorAfterRegistration($user, $student, $contact) {
+		$body = "Dear " . $student->getFirstname() . "\n\n";
+		$body .= "You are registered now, Thank you! \n";
+		$body .= "Username: " . $user->getUsername() . "\n";
+		$body .= "Please login to our website and see your information. \n";
+		$body .= Constants::APPLICATION_DOMAIN."index.php?cmd=public/login";
 		return $body;
 	}
 	
