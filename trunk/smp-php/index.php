01103eeeb118b10618278fba2c0095449aec1537
lsq991 <lsq991@d4836a7a-25a3-11df-a76c-adfa62dee608>
2010-10-01 04:37:45 +0000

    * add some content to the home, cotact, about us page and * relative css style

--- trunk/smp-php/index.php
+++ trunk/smp-php/index.php
@@ -1,6 +1,6 @@
 <?php
 require('smp/Constants.php');
-require('smp/controller/Controller.php'); 
+require('smp/controller/Controller.php');
 
 // Include a local PEAR copy on a shared host
 set_include_path('~/pear/lib' . PATH_SEPARATOR . get_include_path());
--- trunk/smp-php/smp/view/common/footer.php
+++ trunk/smp-php/smp/view/common/footer.php
@@ -1,13 +1,16 @@
-			
+
 			</div><!-- end content -->
 			<div id="footer" class="grid_12">
-				FOOTER OF PAGE
-			</div>
-		</div><!-- end container -->
-		<script type="text/javascript">
+				<iframe width="930px" height="80px" scrolling="auto"
+  					frameborder="0" allowtransparency="true"
+  					src="http://quotes4all.net/quotes.html?quote_align=left&quote_background=49531f&image=none&image_size=40&author_align=left&padding=0&title_align=left">
+				</iframe><br />
+				Copyright&copy; 2010 by smp All Rights Reserved</div>
+			</div><!-- end container -->
+			<script type="text/javascript">
 			<!--
 			var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {imgDown:"static/css/menu/images/SpryMenuBarDownHover.gif", imgRight:"static/css/menu/images/SpryAssets/SpryMenuBarRightHover.gif"});
 			//-->
 		</script>
-	</body>	
+	</body>
 </html>
