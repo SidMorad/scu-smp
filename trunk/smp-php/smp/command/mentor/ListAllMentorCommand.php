d5465ccd2df9ed600b6e97d46c13e798d93845d9
sid.moradi <sid.moradi@d4836a7a-25a3-11df-a76c-adfa62dee608>
2010-10-12 06:13:58 +0000

    * Make the 'expired' field in the search required * Make the 'matched' and 'trained' fields optional (only add to criteria if they are selected).

--- trunk/smp-php/smp/command/mentor/ListAllMentorCommand.php
+++ trunk/smp-php/smp/command/mentor/ListAllMentorCommand.php
@@ -10,12 +10,15 @@ require_once('smp/command/Command.php');
 require_once('smp/service/MentorService.php');
 require_once('smp/domain/Student.php');
 require_once('smp/base/SessionRegistry.php');
+
 class smp_command_mentor_ListAllMentorCommand extends smp_command_Command {
 
 	function doExecute(smp_controller_Request $request) {
 		$mentorService = new smp_service_MentorService();
 
 		$mentor = new smp_domain_Mentor();
+		$mentor->setExpired(false);
+		smp_base_SessionRegistry::setSearchEntity('mentor_ListAllMentor_MentorSearch', $mentor);
 		if ($request->isPost()) {
 			$student = new smp_domain_Student();
 			$student->setFirstname($request->getProperty('firstname'));
@@ -24,7 +27,7 @@ class smp_command_mentor_ListAllMentorCommand extends smp_command_Command {
 			$student->setGender($request->getProperty('gender'));
 			$student->setCourseId($request->getProperty('courseId'));
 			$student->setStudyMode($request->getProperty('studyMode'));
-			$mentor->setExpired((is_null($request->getProperty('expired')) ? null : true));
+			$mentor->setExpired((is_null($request->getProperty('expired')) ? false : true));
 			$mentor->setMatched((is_null($request->getProperty('matched')) ? null : true));
 			$mentor->setTrained((is_null($request->getProperty('trained')) ? null : true));
 			
@@ -36,7 +39,7 @@ class smp_command_mentor_ListAllMentorCommand extends smp_command_Command {
 
 		$outputFormat = $request->getProperty('output_format');
 		if (is_null($outputFormat)) {
-			$datagrid = $mentorService->getAllMentorDatagrid($mentor, true);
+			$datagrid = $mentorService->getAllMentorDatagrid($mentor);
 		} else {
 			$datagrid = $mentorService->getAllMentorDatagrid($mentor, false);
 		}
